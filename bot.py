6feebc6c1ccc0773b3398fca1f877912b5cca0c5
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2026-06-10 23:46:56 +0300

    added modul_2

--- bot.py
+++ bot.py
@@ -1,21 +1,12 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes, CommandHandler
+# from telegram import Update
+from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes, CommandHandler, MessageHandler, filters
 
 from gpt import ChatGptService
-from util import (load_message, load_prompt, send_text, send_image, send_text_buttons, show_main_menu,
-                  default_callback_handler)
+# from util import (load_message, load_prompt, send_text, send_image, send_text_buttons, show_main_menu,
+#                   default_callback_handler)
+from util import *
 import credentials
 import random
-
-# ### 1. *"Випадковий факт"*
-# Телеграм-бот повинен обробляти команду /random.
-# При обробці команди він надсилає заздалегідь підготовлене зображення
-# та робить запит до ChatGPT із заздалегідь підготовленим промптом.
-# Відповідь ChatGPT потрібно отримати та передати користувачеві.
-# До повідомлення має бути прикріплена кнопка "Закінчити", натискання на яку
-# працює так само, як команда /start.
-# І кнопка "Хочу ще факт", натискання на яку
-# працює так само, як команда /random
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -30,7 +21,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         'quiz': 'Взяти участь у квізі ❓'
         # Додати команду в меню можна так:
         # 'command': 'button text'
-
     })
 
 async def random(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -53,13 +43,30 @@ async def random_button_next(update: Update, context):
 
 
 
+async def gpt(update, context):
+    await send_image(update, context, "gpt")
+    gpt_text_load = load_message("gpt")
+    # print(gpt_text_load)
+    await send_text(update, context, gpt_text_load)
+
+async def gpt_dialog(update, context):
+    text = update.message.text
+    # print(text)
+    prompt = load_prompt("gpt")
+    answer = await chat_gpt.send_question(prompt, text)
+    # print(answer)
+    await send_text(update, context, answer)
+
+
 chat_gpt = ChatGptService(credentials.ChatGPT_TOKEN)
 app = ApplicationBuilder().token(credentials.BOT_TOKEN).build()
 
 # Зареєструвати обробник команди можна так:
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("random", random))
-# app.add_handler(CommandHandler("gpt", gpt))
+app.add_handler(CommandHandler("gpt", gpt))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, gpt_dialog))
+
 
 # Зареєструвати обробник колбеку можна так:
 app.add_handler(CallbackQueryHandler(random_button_next, pattern='^random_.*'))
